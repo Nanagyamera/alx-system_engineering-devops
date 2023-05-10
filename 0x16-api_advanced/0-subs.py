49630c446e6871c3f974e959da994e3a9ad48f66
Nanagyamera <nanagyamera3@gmail.com>
2023-05-10 03:05:56 -0700

    API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,9 +8,8 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers,allow_redirects=False)
 
-    if response.status_code == 200:
-        json_data = response.json()
-        return json_data['data']['subscribers']
-    else:
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
 
